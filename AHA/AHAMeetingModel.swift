be8058908e1dedcfe9a0fc4b2a8ba1a45869b446
Justin Warmkessel <justin.w@ymedialabs.com>
2016-05-05 19:40:31 -0700

    Added model for controller

---
+++ AHA/AHAMeetingModel.swift
@@ -0,0 +1,13 @@
+//
+//  AHAMeetingModel.swift
+//  AHA
+//
+//  Created by Justin Warmkessel on 5/5/16.
+//  Copyright © 2016 Justin Warmkessel. All rights reserved.
+//
+
+import UIKit
+
+class AHAMeetingModel: NSObject {
+
+}
