4567938764761469e729824a7e2d51e977863143
Justin Warmkessel <justin.w@ymedialabs.com>
2016-05-05 19:39:12 -0700

    second commit

--- AHA/AHAMeetingViewController.swift
+++ AHA/AHAMeetingViewController.swift
@@ -10,6 +10,82 @@ import UIKit
 
 class AHAMeetingViewController: UIViewController {
 
+    let recorder : AHARecorder = AHARecorder()
+    
+    @IBOutlet weak var tableView: UITableView!
+    //Used to animate the height of the tableView.
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var tableViewBottomLayoutConstraint: NSLayoutConstraint!
+        {
+        didSet{
+            self.tableViewBottomLayoutConstraint.active = false
+        }
+    }
+    
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var doubleTapView: UIView!
+    
+    @IBOutlet weak var recordingView: UIView!
+
+    @IBAction func tapHandler(sender: AnyObject) {
+        
+        print("doubleTap")
+        
+        self.view.bringSubviewToFront(self.recordingView)
+        
+        // func: Create AHA Recorder Object
+        // subfunc: Create meetings directory
+        // subfunc: Create uniqueID directory for this meeting
+        // Start recording
+
+    }
+    
+    @IBAction func snippetCaptureHandler(sender: AnyObject) {
+        print("snippet Cature Tap")
+    }
+    @IBAction func menuButtonHandler(sender: AnyObject, forEvent event: UIEvent) {
+        //TODO FIXME://Animate tableview not working expected manner.
+        //self.toggleListView()
+        
+    }
+    
+    func toggleListView() {
+        self.tableViewHeightConstraint.active = false
+        
+        if (self.tableViewBottomLayoutConstraint == nil)
+        {
+            let verticalConstraint = NSLayoutConstraint(item: self.tableView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+            
+            self.tableView.addConstraint(verticalConstraint)
+        }
+        else
+        {
+            self.tableViewBottomLayoutConstraint.active = true
+            self.tableViewBottomLayoutConstraint.constant = 0
+        }
+        
+        UIView.animateWithDuration(0.4) { () -> Void in
+            
+            
+            self.view.layoutIfNeeded()
+        }
+    }
+  
+    //TODO fix shake to stop
+    override func motionCancelled(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if (motion == .MotionShake)
+        {
+            print("shake")
+        
+        }
+    }
+    
+    //TODO fix shake to stop
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ AHA/AHARecorder.swift
@@ -0,0 +1,100 @@
+//
+//  AHARecorder.swift
+//  AHA
+//
+//  Created by Justin Warmkessel on 5/5/16.
+//  Copyright © 2016 Justin Warmkessel. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+protocol RecordAudioDelegate: class {
+    func didFinishTask(sender: AHARecorder)
+}
+class AHARecorder: NSObject, AVAudioRecorderDelegate {
+    weak var delegate:RecordAudioDelegate?
+    var recordingSession         : AVAudioSession?
+    
+    var movieFileOutput : AVCaptureMovieFileOutput?
+    var dataOutput      : AVCaptureVideoDataOutput?
+    var device          : AVCaptureDevice?
+    var audioDevice     : AVCaptureDevice?
+    
+    let documentsURL                                    = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+    var directoryName   : String?                       = NSUUID().UUIDString
+    var arrayOfVideos   : [AnyObject]                   = [AnyObject]()
+    var isRecording                                     = false
+    var kTimerInterval : NSTimeInterval                 = 0.02
+    var completedVideoURL : NSURL?
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    // func: Create AHA Recorder Object
+    // subfunc: Create meetings directory
+    // subfunc: Create uniqueID directory for this meeting
+    // Start recording
+    
+    func requestAudioRecordingPermission()
+    {
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        if let recordingSession = recordingSession {
+            do {
+                try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+                try recordingSession.setActive(true)
+                recordingSession.requestRecordPermission() { [unowned self] (allowed: Bool) -> Void in
+                    dispatch_async(dispatch_get_main_queue()) {
+                        if allowed {
+//                            self.loadRecordingUI()
+                        } else {
+                            // failed to record!
+                        }
+                    }
+                }
+            } catch {
+                // failed to record!
+            }
+        }
+    }
+    
+    func startRecording() {
+        let documentDirectory : String = getDocumentsDirectory()
+        let audioFilename = documentDirectory.stringByAppendingPathComponent("recording.m4a")
+        let audioURL = NSURL(fileURLWithPath: audioFilename)
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000.0,
+            AVNumberOfChannelsKey: 1 as NSNumber,
+            AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(URL: audioURL, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func getDocumentsDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    func finishRecording(success success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to Re-record", forState: .Normal)
+        } else {
+            recordButton.setTitle("Tap to Record", forState: .Normal)
+            // recording failed :(
+        }
+    }
+}
